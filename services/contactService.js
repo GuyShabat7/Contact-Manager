cc9e8cd2c0358dc1fc7192bf65e5083ed7fde414
doronm <doronm@chain-reaction.io>
2026-06-09 15:51:19 +0300

    refactor: streamline command execution logic in contactService.js

--- services/contactService.js
+++ services/contactService.js
@@ -1,16 +1,14 @@
 
 import { validate } from "./utils/validation.js";
+import { processCommand } from "./commands/commandHnadler.js";
 export const executeCommand = function(command, args){
     //utils validation
     const cmdValidity = validate(command, args);
 
-    if (cmdValidity.status === false){
-        return cmdValidity;
-    }
-    else
+    if (cmdValidity.status === true)
     {
-        //call commands manager
-        return cmdValidity;
+        cmdValidity.status = processCommand(command, args);
     }
+    return cmdValidity;
 }
 
