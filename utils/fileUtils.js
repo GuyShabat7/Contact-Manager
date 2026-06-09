8dfbeb52c92531f169e5e198af573569063cc4c0
doronm <doronm@chain-reaction.io>
2026-06-09 14:30:58 +0300

    add fs module with security version and implement read/write JSON functions

--- utils/fileUtils.js
+++ utils/fileUtils.js
@@ -1,4 +1,25 @@
-export const readJSON = function(filePaths){}
+const fs = require("fs");
 
-export const writeJSON = function(filePath, data){}
+const fileNmae = "contact.json:'";
 
+export const readJSON = function () {
+  let retValue = { status: false, data: null, err: null };
+  try {
+    const data = JSON.parse(fs.readFileSync(fileNmae, "utf8"));
+    retValue = { status: true, data: data, err: null };
+  } catch (err) {
+    retValue.err = err;
+  }
+  return retValue;
+};
+
+export const writeJSON = function (data) {
+  let retValue = { status: false, err: null };
+  try {
+    fs.writeFileSync(fileNmae, JSON.stringify(data));
+    retValue .status = true;
+  } catch (err) {
+    retValue.err = err;
+  }
+  return retValue;
+};
